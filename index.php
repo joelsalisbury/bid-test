6c80a2f700288e285e95eb498e5cef143d97457b
Joel Salisbury <joel.saisbury@gmail.com>
2024-02-03 10:41:30 -0500

    viewport meta

--- index.php
+++ index.php
@@ -35,6 +35,8 @@ if (isset($_POST['name']) && isset($_POST['bid'])) {
 
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bid Testr</title>
     <!-- bootstrap 5 cdn-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
